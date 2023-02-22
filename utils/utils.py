79d4835d91fd32b9ef434d38dbc88077877b0004
user <kosareva.karina2013@yandex.ru>
2023-02-22 19:46:55 +0300

    change utils.py (add __str__, __gt__ and __add__)

--- utils/utils.py
+++ utils/utils.py
@@ -15,15 +15,21 @@ class Youtube:
         self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
         """Инициализация атрибутов класса"""
-        self.title = self.channel['items'][0]['snippet']['title']                          # название канала
-        self.description = self.channel['items'][0]['snippet']['description']              # описание канала
-        self.url = 'https://www.youtube.com/' + self.__channel_id                          # ссылка на канал
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']  # количество подписчиков
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']            # количество видео
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']              # общее количество просмотров
+        # название канала
+        self.title = self.channel['items'][0]['snippet']['title']
+        # описание канала
+        self.description = self.channel['items'][0]['snippet']['description']
+        # ссылка на канал
+        self.url = 'https://www.youtube.com/' + self.__channel_id
+        # количество подписчиков
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        # количество видео
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        # общее количество просмотров
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     @property
-    def channel_id(self):
+    def channel_id(self) -> str:
         """Получение id канала"""
         return self.__channel_id
 
@@ -32,16 +38,33 @@ class Youtube:
         with open(name_json, 'w', encoding='UTF=8') as file:
             data = {
                 'id': self.__channel_id, 'title': self.title, 'description': self.description, 'url': self.url,
-                'subscriber_count': self.subscriber_count, 'video_count': self.video_count, 'view_count': self.view_count
-                }
+                'subscriber_count': self.subscriber_count, 'video_count': self.video_count,
+                'view_count': self.view_count
+            }
             return json.dump(data, file, indent=2, ensure_ascii=False)
+
     @staticmethod
-    def get_service():
+    def get_service() -> object:
         """Возвращает объект для работы с API ютуба"""
         load_dotenv()
         api_key: str = os.getenv('YT_API_KEY')  # получение ключа из файла .env
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
+
     def print_info(self):
         """Вывод инфорации о ютуб-канале в консоль"""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def __str__(self) -> str:
+        """Возвращает информацию о канале (название канала)"""
+        return f'Youtube-канал: {self.title}'
+
+    def __gt__(self, other) -> bool:
+        """Сравнивает количество подписчиков"""
+        if isinstance(other, Youtube):
+            return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __add__(self, other) -> int:
+        """Суммирует количество подписчиков"""
+        if isinstance(other, Youtube):
+            return int(self.subscriber_count) + int(other.subscriber_count)
